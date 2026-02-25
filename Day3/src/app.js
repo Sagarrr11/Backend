1b8e87421bfaddcb3aa1a73a2bd947b95812d91f
Sagarrr11 <sagarrr0511@gmail.com>
2026-02-25 17:58:11 +0530

    Day-3 done

---
+++ Day3/src/app.js
@@ -0,0 +1,41 @@
+/* 
+    -server create krna
+    -server ko config
+
+*/
+
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.post("/notes",(req,res)=>{
+    res.send(req.body)
+    notes.push(req.body)
+    
+})
+
+app.get("/notes",(req,res)=>{
+    res.send(notes)
+})
+
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index]
+    res.send("note deleted successfully")
+
+})
+
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description = req.body.description
+    res.send("note updated successfull ")
+})
+
+
+module.exports = app
