1c60781e699d98c708bafa26a62c61e8ce51dc66
Sagarrr11 <sagarrr0511@gmail.com>
2026-03-02 09:57:09 +0530

    DAy9PracticeDatabse

---
+++ Day9/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const noteModel = require("./models/noteModel")
+
+app.post("/notes",async (req,res)=>{
+    const {title , description} = req.body
+
+    const note = await noteModel.create({
+        title , description
+    })
+
+    res.send(200).json({
+        message: "Note is created successfully",
+        note
+    })
+})
+
+app.get("/notes",async (req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(201).json({
+        message: "Note fetched successfully",
+        notes
+    })
+})
+
+module.exports = app
